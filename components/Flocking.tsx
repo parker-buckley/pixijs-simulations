f03e06d14b1aa723ff102e052362011e244ba5af
Parker Buckley <parker.brett.buckley@gmail.com>
2025-04-05 17:29:14 -0500

    Clean up BG initialization

--- components/Flocking.tsx
+++ components/Flocking.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from "react";
 import * as PIXI from "pixi.js";
 
 import { Boid, Vector } from './types/flocking/flocking.types'
-import { calculateCohesion, calculateSeparation, calculateAlignment, boundPositions } from '@/lib/flocking/flocking'
+import { calculateCohesion, calculateSeparation, calculateAlignment, boundPositions, setupBackground } from '@/lib/flocking/flocking'
 import { getRandomSignedNumber } from "@/lib/utils";
 
 const Flocking = () => {
@@ -73,47 +73,7 @@ const Flocking = () => {
         }
         pixiContainerRef.current.appendChild(app.canvas);
 
-
-        const [ bg1, bg2 , bg3, bg4 ] = await Promise.all( [
-          PIXI.Assets.load<PIXI.Texture>('/BG/1.png')
-          , PIXI.Assets.load<PIXI.Texture>('/BG/2.png')
-          , PIXI.Assets.load<PIXI.Texture>('/BG/3.png')
-          , PIXI.Assets.load<PIXI.Texture>('/BG/4.png')
-        ]);
-
-        const bg1Sprite = new PIXI.Sprite( bg1 );
-        const bg2Sprite = new PIXI.Sprite( bg2 );
-        const bg3Sprite = new PIXI.Sprite( bg3 );
-        const bg4Sprite = new PIXI.Sprite( bg4 );
-
-        bg1Sprite.height = appRef.current.screen.height;
-        bg1Sprite.width = appRef.current.screen.width;
-        bg2Sprite.height = appRef.current.screen.height;
-        bg2Sprite.width = appRef.current.screen.width;
-        bg3Sprite.height = appRef.current.screen.height;
-        bg3Sprite.width = appRef.current.screen.width;
-        bg4Sprite.height = appRef.current.screen.height;
-        bg4Sprite.width = appRef.current.screen.width;
-
-        bg1Sprite.anchor = 0.5;
-        bg2Sprite.anchor = 0.5;
-        bg3Sprite.anchor = 0.5;
-        bg4Sprite.anchor = 0.5;
-
-        bg1Sprite.x = appRef.current.screen.width / 2;
-        bg1Sprite.y = appRef.current.screen.height / 2;
-        bg2Sprite.x = appRef.current.screen.width / 2;
-        bg2Sprite.y = appRef.current.screen.height / 2;
-        bg3Sprite.x = appRef.current.screen.width / 2;
-        bg3Sprite.y = appRef.current.screen.height / 2;
-        bg4Sprite.x = appRef.current.screen.width / 2;
-        bg4Sprite.y = appRef.current.screen.height / 2;
-
-        appRef.current.stage.addChild(bg1Sprite);
-        appRef.current.stage.addChild(bg2Sprite);
-        appRef.current.stage.addChild(bg3Sprite);
-        appRef.current.stage.addChild(bg4Sprite);
-
+        await setupBackground( appRef );
 
         const [ birdFlightFrame1, birdFlightFrame2 , birdFlightFrame3, birdFlightFrame4 ] = await Promise.all( [
           PIXI.Assets.load<PIXI.Texture>('/Animations/birdFlight/flight1.png')
--- lib/flocking/flocking.ts
+++ lib/flocking/flocking.ts
@@ -1,3 +1,4 @@
+import * as PIXI from 'pixi.js'
 import { Boid, Vector } from '../../components/types/flocking/flocking.types'
 
 const SPEED_LIMIT = 3;
@@ -130,4 +131,48 @@ export const boundPositions = (boids: Boid[], xMin: number, xMax: number, yMin: 
             boid.velocity.y += 3;
         }
     } 
+}
+
+export const setupBackground = async ( appRef: React.MutableRefObject<PIXI.Application<PIXI.Renderer> | null>) => {
+    if( !appRef.current ) return;
+
+    const [ bg1, bg2 , bg3, bg4 ] = await Promise.all( [
+        PIXI.Assets.load<PIXI.Texture>('/BG/1.png')
+        , PIXI.Assets.load<PIXI.Texture>('/BG/2.png')
+        , PIXI.Assets.load<PIXI.Texture>('/BG/3.png')
+        , PIXI.Assets.load<PIXI.Texture>('/BG/4.png')
+    ]);
+
+    const bg1Sprite = new PIXI.Sprite( bg1 );
+    const bg2Sprite = new PIXI.Sprite( bg2 );
+    const bg3Sprite = new PIXI.Sprite( bg3 );
+    const bg4Sprite = new PIXI.Sprite( bg4 );
+
+    bg1Sprite.height = appRef.current.screen.height;
+    bg1Sprite.width = appRef.current.screen.width;
+    bg2Sprite.height = appRef.current.screen.height;
+    bg2Sprite.width = appRef.current.screen.width;
+    bg3Sprite.height = appRef.current.screen.height;
+    bg3Sprite.width = appRef.current.screen.width;
+    bg4Sprite.height = appRef.current.screen.height;
+    bg4Sprite.width = appRef.current.screen.width;
+
+    bg1Sprite.anchor = 0.5;
+    bg2Sprite.anchor = 0.5;
+    bg3Sprite.anchor = 0.5;
+    bg4Sprite.anchor = 0.5;
+
+    bg1Sprite.x = appRef.current.screen.width / 2;
+    bg1Sprite.y = appRef.current.screen.height / 2;
+    bg2Sprite.x = appRef.current.screen.width / 2;
+    bg2Sprite.y = appRef.current.screen.height / 2;
+    bg3Sprite.x = appRef.current.screen.width / 2;
+    bg3Sprite.y = appRef.current.screen.height / 2;
+    bg4Sprite.x = appRef.current.screen.width / 2;
+    bg4Sprite.y = appRef.current.screen.height / 2;
+
+    appRef.current.stage.addChild(bg1Sprite);
+    appRef.current.stage.addChild(bg2Sprite);
+    appRef.current.stage.addChild(bg3Sprite);
+    appRef.current.stage.addChild(bg4Sprite);
 }
